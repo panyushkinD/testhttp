a71497fca3b1b7b1f494d1f43b1eeada93d30a50
panyushkin <danila1238@mail.ru>
2024-05-08 23:07:29 +0300

    Add crud_methods

--- src/main/java/com/example/demo/service/UsersService.java
+++ src/main/java/com/example/demo/service/UsersService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UsersRequestDto;
 import com.example.demo.dto.UsersResponseDto;
+import com.example.demo.model.Users;
 import com.example.demo.repository.UsersRepository;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import org.apache.coyote.Request;
 import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -27,4 +31,27 @@ public class UsersService {
                         .build()
         ).collect(Collectors.toList());
     }
+
+    public Users getUserById(int id) {
+        return usersRepository.findUsersById(id);
+    }
+    public Users deleteUsersById (int id) {
+        return usersRepository.deleteById(id);
+    }
+
+
+
+
+    public void addNewUser(UsersRequestDto usersRequestDto)  {
+        Users insertUser = Users.builder()
+                .firstName(usersRequestDto.getFirstName())
+                .lastName(usersRequestDto.getLastName())
+                .patronymicName(usersRequestDto.getPatronymicName())
+                .login(usersRequestDto.getLogin())
+                .password(usersRequestDto.getPassword())
+                .age(usersRequestDto.getAge())
+                .build();
+        usersRepository.save(insertUser);
+    }
+
 }
